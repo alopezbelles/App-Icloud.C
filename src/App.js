86a4f86a3dd5b700a9b80541655983e3900fa887
Alex <alopezbelles@gmail.com>
2023-04-01 13:50:01 +0200

    structure navbar and header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home/Home";
 import Login from "./Containers/Login/Login";
 import Register from "./Containers/Register/Register";
-import Register from "./Containers/Documents/Documents";
+import Documents from "./Containers/Documents/Documents";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 // BOOTSTRAP
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 // STYLES AND AESSETS
-
 import "./Header.css";
 
 
 function Header() {
+    
+    const navigate = useNavigate();
+   
 
-    return(
-        <div></div>
-    )
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      
+      
+    </>
+  );
 }
+
 
 export default Header;
