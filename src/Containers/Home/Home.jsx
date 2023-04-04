3733c33316bb694c6782f263c2554d3039678926
Alex <alopezbelles@gmail.com>
2023-04-04 17:41:29 +0200

    navbar size change

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -20,26 +20,35 @@ function Home() {
       <Col className="col1Design">
         <Image src={logo} className="logoHome" alt="Image1"></Image>
         <h3 className="subtittleLogo">
-        Share Security is a fictional software security company that provides top-notch solutions to businesses and individuals. Their software offers comprehensive protection against various cyber threats, including malware, ransomware, and phishing attacks. Share Security prides itself on its advanced technology, which allows for real-time monitoring and threat detection. 
+          Share Security is a fictional software security company that provides
+          top-notch solutions to businesses and individuals. Their software
+          offers comprehensive protection against various cyber threats,
+          including malware, ransomware, and phishing attacks. Share Security
+          prides itself on its advanced technology, which allows for real-time
+          monitoring and threat detection.
         </h3>
-        <Row className="rowButtons">
+      </Col>
+      <Col className="colButtons">
+        
           <div onClick={() => navigate("/login")}>
             <Button text="LOGIN"></Button>
           </div>
           <div onClick={() => navigate("/register")}>
             <Button text="REGISTER"></Button>
           </div>
-        </Row>
+        
       </Col>
       <Col className="col2Design">
         <Image src={vectors} className="vectorsDesign" alt="Image2"></Image>
       </Col>
       <Col className="col4Design">
         <h3 className="subtittleLogo">
-        The company also offers regular software updates and excellent customer support to ensure that its clients are always secure. 
+          The company also offers regular software updates and excellent
+          customer support to ensure that its clients are always secure.
         </h3>
         <h3 className="subtittleLogo">
-        Overall, Share Security is a trusted and reliable choice for anyone looking to safeguard their digital assets.
+          Overall, Share Security is a trusted and reliable choice for anyone
+          looking to safeguard their digital assets.
         </h3>
       </Col>
     </Container>
